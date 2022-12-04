4a3dbbec8641523f9f96f1a9bd152176ee1ff88c
PISSEUR DE CODE <pierre.castets@gmail.com>
2022-12-04 13:15:49 +0100

    Création de la fonction de calcul en polonaise inversée.

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -27,6 +27,66 @@ namespace TestNotationPolonaise
             } while (reponse != carac1 && reponse != carac2);
             return reponse;
         }
+        static float polonaise(String formule)
+        {
+            try
+            {
+                string[] t = formule.Split(' ');
+                int nbCases = t.Length;
+                while (nbCases > 1)
+                {
+                    // recherche d'un opérateur
+                    int k = t.Length - 1;
+                    while (t[k] != "+" && t[k] != "-" && t[k] != "/" && t[k] != "*" && k > 0)
+                    {
+                        k--;
+                    }
+                    // calcul du résultat
+                    float result = 0;
+                    float a = float.Parse(t[k + 1]);
+                    float b = float.Parse(t[k + 2]);
+                    switch (t[k])
+                    {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "/":
+                            if (b == 0)
+                            {
+                                return float.NaN;
+                            }
+                            result = a / b;
+                            break;
+                        case "*":
+                            result = a *b;
+                            break;
+                    }
+                    // stockage du résultat à la place de l'opérateur
+                    t[k] = result.ToString();
+
+                    // suppression des 2 cases suivantes
+                    for (int i = k + 1; i < nbCases - 2; i++)
+                    {
+                        t[i] = t[i + 2];
+                    }
+                    // case suivantes mises à blanc
+                    for (int i = nbCases - 2; i < nbCases; i++)
+                    {
+                        t[i] = "";
+                    }
+                    nbCases -= 2;
+                }
+                return float.Parse(t[0]);
+            }
+            catch
+            {
+                Console.WriteLine("Erreur de saisie");
+                return float.NaN;
+            }
+        }
 
         /// <summary>
         /// Saisie de formules en notation polonaise pour tester la fonction de calcul
@@ -40,9 +100,9 @@ namespace TestNotationPolonaise
             {
                 Console.WriteLine();
                 Console.WriteLine("entrez une formule polonaise en séparant chaque partie par un espace = ");
-                string laFormule = Console.ReadLine();
+                String laFormule = Console.ReadLine();
                 // affichage du résultat
-                Console.WriteLine("Résultat =  " + Polonaise(laFormule));
+                Console.WriteLine("Résultat =  " + polonaise(laFormule));
                 reponse = saisie("Voulez-vous continuer ?", 'O', 'N');
             } while (reponse == 'O');
         }
